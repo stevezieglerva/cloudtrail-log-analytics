346740c4ad524289380649e2878afb5634bb28fb
Steve Ziegler <stephen.v.ziegler@gmail.com>
2019-11-07 11:57:27 -0500

    updated init

--- cf_build.sh
+++
@@ -1,3 +0,0 @@
-sam validate -t template.yaml
-sam build
-sam package --output-template-file packaged.yaml --s3-bucket svz-sam
---
+++ sam-app/cf_build.sh
@@ -0,0 +1,4 @@
+sam validate -t template.yaml
+sam build
+sam package --output-template-file packaged.yaml --s3-bucket svz-sam
+sam deploy --template-file packaged.yaml --stack-name cloudtrail-log-analytics --capabilities CAPABILITY_IAM
